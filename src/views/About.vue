e0a82bf901a44686c15a849345a07855d6c67179
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2022-04-20 14:19:54 +0530

    Added LinkedIn Card To About

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,8 +3,8 @@
    <div class="container">
             <div class="row">
                 <div class="col-md-6 center">
-                    <img class="profile" style="" :src="require('../assets/'+imageUrl)" alt="">
-                    
+                    <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="abhishek-bhardwaj-191b9784" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/abhishek-bhardwaj-191b9784?trk=profile-badge">Abhishek Bhardwaj</a></div>
+              
                 </div>
                 <div class="col-md-6 text-left center-ver text-dark-cyan ">
                     
