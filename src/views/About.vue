b35f4333c440476282695afc2fb6e5590036ad18
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2022-04-20 14:38:43 +0530

    Significant changes in about text

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,8 @@
    <div class="container">
             <div class="row">
                 <div class="col-md-6 center">
-                    <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="abhishek-bhardwaj-191b9784" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/abhishek-bhardwaj-191b9784?trk=profile-badge"></div>
+                    <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="abhishek-bhardwaj-191b9784" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/abhishek-bhardwaj-191b9784?trk=profile-badge"></a></div>
+              
               
                 </div>
                 <div class="col-md-6 text-left center-ver text-dark-cyan ">
@@ -12,10 +13,8 @@
                         
                         <h1 class="text-dark-cyan">About Me</h1>
                             
-                            <span class='text-white '>My name is Abhishek Bhardwaj and i am 
-                                {{ new Date().getFullYear()-1995 }} Years old, i have a polytechnic diploma in <b class="text-dark-cyan">electronics and communication engineering</b> which was completed in the year of 2017, i also have a Degree of <b class="text-dark-cyan">Bachelors of
-                            computer application</b> from IGNOU in the year of 2018, i am a tech enthusiast who want to know about new technologies but more interested in <b class="text-dark-cyan">website designing and development</b>,i also maintain a facebook page with around thousand
-                            likes i also manage tech blog mention inside projects, i like to play video game and listening songs,to Know More About me there are navigation links.</span>
+                            <span class='text-white '>My name is Abhishek Bhardwaj and I am a Website Designer and Developer, i have made several websites and <router-link to="/work">projects</router-link> for me and for my company in which i worked and as freelancer too.<br> I also have a polytechnic diploma in <b class="text-dark-cyan">Electronics and Communication Engineering</b> which was completed in the year of 2017, i also have a Degree of <b class="text-dark-cyan">Bachelors of
+                            Computer Application</b> from IGNOU in the year of 2018, i am a tech enthusiast who want to know about new technologies but more interested in <b class="text-dark-cyan">website designing and development</b>As hobby i like to play video game and listening songs,</span>
                     </div>
                    
                 </div>
