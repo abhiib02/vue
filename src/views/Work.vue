00e9f6b06d5a6d73f58e05270623b42e1a312d86
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2022-07-30 13:04:52 +0530

    added carousel for test

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -79,12 +79,16 @@
 
 
 
-
+<Carousel :carousalImages="carousalImages"></Carousel>
 
 </template>
 <script>
+import Carousel from '@/components/Carousel.vue';
 
   export default {
+    components:{
+    Carousel
+  },
     data() {
       return {
         projects: [
@@ -297,7 +301,57 @@
             Status: 'Online',
             Published : "Aug 2013"
           }
-        ]
+        ],
+        carousalImages: [
+        {
+          image:
+            "https://images.unsplash.com/photo-1606945094026-f93bf0b75ee0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          captionText: "Caption 1",
+          subCaption: "lorem ipsume dolor polor asdaasdasdasd dwqdacads",
+        },
+        {
+          image:
+            "https://images.unsplash.com/photo-1606922619208-e0a4227607f6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=376&q=80",
+          captionText: "Caption 2",
+          subCaption: "lorem ipsume dolor polor asdaasdasdasd dwqdacads",
+        },
+        {
+          image:
+            "https://images.unsplash.com/photo-1601758176559-76c75ead317a?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          captionText: "Caption 3",
+          subCaption: "lorem ipsume dolor polor asdaasdasdasd dwqdacads",
+        },
+        {
+          image:
+            "https://images.unsplash.com/photo-1606922722933-550af9f7e4de?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          captionText: "Caption 4",
+          subCaption: "lorem ipsume dolor polor asdaasdasdasd dwqdacads",
+        },
+        {
+          image:
+            "https://images.unsplash.com/photo-1606854385394-14d09ac9e14e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80",
+          captionText: "Caption 5",
+          subCaption: "lorem ipsume dolor polor asdaasdasdasd dwqdacads",
+        },
+        {
+          image:
+            "https://images.unsplash.com/photo-1606895213457-36798be25021?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          captionText: "Caption 6",
+          subCaption: "lorem ipsume dolor polor asdaasdasdasd dwqdacads",
+        },
+        {
+          image:
+            "https://images.unsplash.com/photo-1606825488982-e468cab54f26?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          captionText: "Caption 7",
+          subCaption: "lorem ipsume dolor polor asdaasdasdasd dwqdacads",
+        },
+        {
+          image:
+            "https://images.unsplash.com/photo-1606902965483-3dcbb360cca5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          captionText: "Caption 8",
+          subCaption: "lorem ipsume dolor polor asdaasdasdasd dwqdacads",
+        },
+      ],
       }
     }
     ,
