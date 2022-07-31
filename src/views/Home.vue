269f222466d6c61b349dd2c88c494b135772e1ea
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2022-07-31 16:18:49 +0530

    added interactive logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,12 +24,48 @@
 
                 </div>
                 <div class="col-sm-6 center hide-on-mobile">
-                    <img class="logo-main" src="../assets/logo.svg" alt="">
+                    <img class="logo-main" v-bind:style="dropshades" src="../assets/logo.svg" alt="">
                 </div>
             </div>
         </div>
   </div>
 </template>
+<script>
+export default {
+    data(){
+        return {
+            x:'',
+            y:''
+        }
+    }
+    methods:{
+        mouseinvert(event){
+            var x3d,y3d;
+            x3d = event.clientX-(window.innerWidth * 0.5);
+            y3d = event.clientY-(window.innerHeight * 0.5);
+            x3d = x3d * 0.1;
+            y3d = y3d * 0.1;
+            this.x = x3d;
+            this.y = y3d;
+        }
+    },
+    computed:{
+        dropshades(){
+            return `filter:brightness(0)
+          drop-shadow(${this.x}px ${this.y}px 1px rgba(${this.x} * 2},${this.y} * 2},${this.x} * y * 2})) 
+          drop-shadow(${this.x}px ${this.y}px 0px rgba(${this.x} * 4},${this.y} * 3},${this.x} * y * 3})) 
+          drop-shadow(${this.x}px ${this.y}px 0px rgba(${this.x} * 6},${this.y} * 4},${this.x} * y * 4})) 
+          drop-shadow(${this.x}px ${this.y}px 0px rgba(${this.x} * 8},${this.y} * 5},${this.x} * y * 5})) 
+          drop-shadow(${this.x}px ${this.y}px 0px rgba(${this.x} * 10},${this.y} * 6},${this.x} * y * 6})) 
+          drop-shadow(${this.x}px ${this.y}px 0px rgba(${this.x} * 12},${this.y} * 7},${this.x} * y * 7})) 
+          drop-shadow(${this.x}px ${this.y}px 0px rgba(${this.x} * 14},${this.y} * 8},${this.x} * y * 8})) 
+          drop-shadow(${this.x}px ${this.y}px 0px rgba(${this.x} * 16},${this.y} * 9},${
+            x * y * 9
+          }));`;
+        }
+    }
+}
+</script>
 <style scoped>
 h1{
     font-weight: bold;
